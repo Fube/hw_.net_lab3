54574d70ba78bf4964c0ddf42f38cb27a3ffe6ac
Fube <fubeitch@gmail.com>
2021-08-31 16:57:25 -0400

    Added nullable to model

--- Models/Tip.cs
+++ Models/Tip.cs
@@ -7,8 +7,8 @@ namespace hw_.net_lab3.Models
         [Required(ErrorMessage = "Cost of meal is required")]
         // double.Epsilon is the smallest positive number
         [Range(double.Epsilon, double.MaxValue, ErrorMessage = "Cost of meal must be greater than 0")]
-        public decimal Cost { get; set; }
+        public decimal? Cost { get; set; }
 
-        public decimal GetTip(decimal Percentage) => Cost * Percentage/100;
+        public decimal GetTip(decimal Percentage) => (Cost ?? 0) * Percentage/100;
     }
 }
