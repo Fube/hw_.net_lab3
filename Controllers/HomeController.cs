84d7945e71bb75f627b6502e2e45a4f56ab88559
Fube <fubeitch@gmail.com>
2021-08-31 15:32:32 -0400

    Init commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using hw_.net_lab3.Models;
+
+namespace hw_.net_lab3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            // Return the view with default values
+            return View(new Tip());
+        }
+
+        [HttpPost]
+        public IActionResult Index(Tip model)
+        {
+            // If valid, return the view with the populated model
+            if(ModelState.IsValid) return View(model);
+    
+            // Otherwise, return the view with the default values
+            return View(new Tip());
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
